ccb93af64528b350dfa337983b0318b6c4b10c6d
Pranit5895 <53489662+Pranit5895@users.noreply.github.com>
2021-01-12 19:55:49 -0800

    code for printing user input name

---
+++ name.java
@@ -0,0 +1,15 @@
+package javaapplication5;
+
+import java.util.Scanner;
+
+public class javaapplication5 {
+
+  public static void main (String[] args) {
+      int arr[] = new int[10];
+      System.out.println("Enter your name : ");
+      string name = new String();
+      Scanner s = new Scanner(System.in);
+      name = s.nextline();
+      System.out.println(name);
+  }
+}
