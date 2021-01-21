2b3a0e0b46bbd03c329412569401fd9d71301e0e
Pranit5895 <guptapranit9@gmail.com>
2021-01-21 16:04:55 +0530

    Variables and packages defined

---
+++ Employee.java
@@ -0,0 +1,13 @@
+package Employee;
+
+import java.util.Scanner;
+
+public class Employee {
+	String name;
+	int year_of_joining;
+	String address;
+
+	public void getInput() {
+
+		Scanner in = new Scanner(System.in);
+
